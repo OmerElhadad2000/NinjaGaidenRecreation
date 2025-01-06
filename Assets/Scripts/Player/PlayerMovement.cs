2fa6113d2ada1718056414599327557f62638e44
omerelhadad2000 <omer.elh11@gmail.com>
2025-01-07 00:24:53 +0200

    Starting To Add Player Wall Slide & Jump Mechanic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -21,85 +21,66 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    
+    [SerializeField] private LayerMask wallLayer;
+    
+    [SerializeField] private Transform wallCheck;
+    
+    
 
     private bool _isJumping;
     
-    private bool _currentlyJumping;
-
     private bool _isInAir;
 
     private bool _isCrouching;
 
     private float _lastXDirection;
 
+    private bool _isWallSliding;
+    
+    private float _wallSlidingSpeed = 2f;
+    
+    private float xInput;
+    
+    
+
     [SerializeField] private Rigidbody2D playerRb;
 
     private void Update()
     {
+        xInput = Input.GetAxis("Horizontal");
+        
         _isJumping = IsOnGround() && Input.GetKey(KeyCode.UpArrow);
         
         _isCrouching = Input.GetKey(KeyCode.DownArrow) && !_isInAir;
         
-        animator.SetBool("Crouching", _isCrouching);
-        
+        WallSlide();
     }
 
     private void FixedUpdate()
     {
-        float xInput = Input.GetAxis("Horizontal");
+        
         
         
         
         // Move the player horizontally only if not crouching
         playerRb.linearVelocity = new Vector2(xInput * moveSpeed, playerRb.linearVelocity.y);
 
-        if (_isCrouching)
+        if (xInput != 0)
         {
-            Crouch();
+            _lastXDirection = xInput;
         }
-        else
-        {
-            if (!_isJumping && playerRb.linearVelocity.x is > 3f or < -3f)
-            {
-                animator.SetBool("Running", true);
-            }
-            else
-            {
-                animator.SetBool("Running", false);
-            }
-            
-            if (xInput != 0)
-            {
-                _lastXDirection = xInput;
-            }
-        }
-
-        if (_lastXDirection != 0)
-        {
-            Vector3 localScale = transform.localScale;
-            localScale.x = Mathf.Sign(_lastXDirection) * Mathf.Abs(localScale.x);
-            transform.localScale = localScale;
-        }
-
-        // Check if the up key is pressed and apply an impulse force if grounded
+        
+        SetPlayerDirection();
+        
         if (_isJumping)
         {
-            
-            
-            animator.SetBool("Ground", false);
-            animator.SetBool(Jumping, _isJumping);
-            // Reset the Y velocity before jumping
-
             JumpNoSword();
-            // animator.SetBool(Running, false);
-            
-
-            // flip the player if moving in the opposite direction
-            
         }
-        else
+        
+        else if (_isCrouching)
         {
-            animator.SetBool("Ground", true);
+            Crouch();
         }
     }
 
@@ -114,15 +95,39 @@ public class PlayerMovement : MonoBehaviour
         playerRb.linearVelocity = new Vector2(0, playerRb.linearVelocity.y);
     }
 
-    private void Run()
-    {
-        
-    }
-
     private bool IsOnGround()
     {
         var hit = Physics2D.Raycast(playerGroundPosition.position, Vector2.down, 0.2f, groundLayer);
-        _isInAir = hit.collider == null;
         return hit.collider != null;
     }
+    
+    private void SetPlayerDirection()
+    {
+        if (_lastXDirection == 0) {return;}
+        Vector3 localScale = transform.localScale;
+        localScale.x = Mathf.Sign(_lastXDirection) * Mathf.Abs(localScale.x);
+        transform.localScale = localScale;
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+        
+    }
+    
+    private void WallSlide()
+    {
+        if (IsWalled() && !IsOnGround() && xInput != 0f)
+        {
+            Debug.Log("Wall sliding");
+            _isWallSliding = true;
+            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, Mathf.Clamp(playerRb.linearVelocity.y, -_wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            _isWallSliding = false;
+        }
+    }
+    
+    
 }
