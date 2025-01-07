b55141784afd0e9a13ae0f8a444e84da41af5f4f
omerelhadad2000 <omer.elh11@gmail.com>
2025-01-07 19:57:50 +0200

    Added To Player Animation A Triggrable Sword
    Added A Sword That Can Hit Enemies At The Right Time (BoxCollider2D That Is A Trigger)

--- Assets/Scripts/Player/PlayerAttacks.cs
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
-{   
+{
+
+    [SerializeField]
+    private BoxCollider2D playerSword;
     
+    private int _mana;
+    private void JumpSwordAttack()
+    {
+        //Attack logic
+    }
+    
+    private void ShuriKenAttack()
+    {
+        //Attack logic
+    }
+    
+    private void SpecialShuriKenAttack()
+    {
+        //Attack logic
+    }
+    
+    private void FireBallAttack()
+    {
+        //Attack logic
+    }
+    
+    private void FireCircleAttack()
+    {
+        //Attack logic
+    }
 }
